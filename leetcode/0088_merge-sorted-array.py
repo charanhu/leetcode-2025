20a4317511bd5dfe66762d0e40e04753bba44a97
charanhu <charanhumail@gmail.com>
2025-01-20 10:52:05 +0530

    Merge Sorted Array

--- leetcode/0088_merge-sorted-array.py
+++ leetcode/0088_merge-sorted-array.py
@@ -7,11 +7,22 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(m, m+n):
-            nums1[i]=nums2[i-m]
+        p1 = m - 1
+        p2 = n - 1
+        write = m + n - 1
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[write] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[write] = nums2[p2]
+                p2 -= 1
+            write -= 1
 
-        nums1=nums1.sort()
-
+        while p2 >= 0:
+            nums1[write] = nums2[p2]
+            p2 -= 1
+            write -= 1
 
 
 # 88. Merge Sorted Array
@@ -26,7 +37,6 @@ class Solution(object):
 
 # The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
- 
 
 # Example 1:
 
@@ -47,7 +57,7 @@ class Solution(object):
 # Explanation: The arrays we are merging are [] and [1].
 # The result of the merge is [1].
 # Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
- 
+
 
 # Constraints:
 
@@ -56,7 +66,7 @@ class Solution(object):
 # 0 <= m, n <= 200
 # 1 <= m + n <= 200
 # -109 <= nums1[i], nums2[j] <= 109
- 
+
 
 # Follow up: Can you come up with an algorithm that runs in O(m + n) time?
 
