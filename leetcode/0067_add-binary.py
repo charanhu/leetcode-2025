577da0474e4337e3fa627b4a3b46210b0621ba86
Charana H U <Charana.H.U@ibm.com>
2025-02-21 21:57:09 +0530

    # 67. Add Binary

---
+++ leetcode/0067_add-binary.py
@@ -0,0 +1,55 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        return bin(int(a, 2)+int(b, 2))[2:]
+        
+
+# 67. Add Binary
+# Solved
+# Easy
+# Topics
+# Companies
+# Given two binary strings a and b, return their sum as a binary string.
+
+ 
+
+# Example 1:
+
+# Input: a = "11", b = "1"
+# Output: "100"
+# Example 2:
+
+# Input: a = "1010", b = "1011"
+# Output: "10101"
+ 
+
+# Constraints:
+
+# 1 <= a.length, b.length <= 104
+# a and b consist only of '0' or '1' characters.
+# Each string does not contain leading zeros except for the zero itself.
+# Seen this question in a real interview before?
+# 1/5
+# Yes
+# No
+# Accepted
+# 1.8M
+# Submissions
+# 3.2M
+# Acceptance Rate
+# 55.1%
+# Topics
+# Math
+# String
+# Bit Manipulation
+# Simulation
+# Companies
+# Similar Questions
+# Add Two Numbers
+# Medium
+# Multiply Strings
+# Medium
+# Plus One
+# Easy
+# Add to Array-Form of Integer
+# Easy
+# Discussion (195)
